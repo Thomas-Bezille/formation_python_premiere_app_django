7ecb8f12764774ed203611cf0fa60d0e2fca40d2
thomas <bezille.thomasb@gmail.com>
2024-12-14 16:24:45 +0100

    📦 NEW: Add datas in a template

--- src/DocBlog/views.py
+++ src/DocBlog/views.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", context={"date": datetime.today()})

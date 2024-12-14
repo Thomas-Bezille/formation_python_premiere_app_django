69ea4a22c3cb102eba3b1ae472ccbdd4b0b168b3
thomas <bezille.thomasb@gmail.com>
2024-12-14 17:16:13 +0100

    📦 NEW: Add views for articles

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import article, index
 
 
 urlpatterns = [
-    path('', index, name="blog-index")
+    path('', index, name="blog-index"),
+    path('article-<str:number_article>/', article, name="article")
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def article(request, number_article):
+    return render(request, f"blog/article_{number_article}.html")
